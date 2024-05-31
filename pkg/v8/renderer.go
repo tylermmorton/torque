5a6bd427be5eb6f8b0857f7c4c91cc91710e0d69
tylermorton <tylermmorton@gmail.com>
2024-05-30 23:29:49 -0400

    Clean up, add basic tracing to v8 renderer

--- pkg/v8/renderer.go
+++ pkg/v8/renderer.go
@@ -3,6 +3,7 @@ package v8
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"rogchap.com/v8go"
 )
@@ -28,23 +29,30 @@ func (r *Renderer) Render(vm interface{}) (string, error) {
 	iso := r.pool.Get()
 	defer r.pool.Put(iso)
 
+	start := time.Now()
 	ctx := v8go.NewContext(iso.Isolate)
 	defer ctx.Close()
+	fmt.Printf("NewContext took: %dms\n", time.Since(start).Milliseconds())
 
+	start = time.Now()
 	_, err := iso.RenderScript.Run(ctx)
 	if err != nil {
 		return "", err
 	}
+	fmt.Printf("Run took: %dms\n", time.Since(start).Milliseconds())
 
 	if !ctx.Global().Has("Render") {
 		return "", fmt.Errorf("function Render() is not defined")
 	}
 
+	start = time.Now()
 	props, err := json.Marshal(vm)
 	if err != nil {
 		return "", err
 	}
+	fmt.Printf("Marshal took: %dms\n", time.Since(start).Milliseconds())
 
+	start = time.Now()
 	renderScript := fmt.Sprintf(`Render(%q)`, string(props))
 	renderResult, err := ctx.RunScript(renderScript, r.scriptName)
 	if err != nil {
@@ -53,11 +61,16 @@ func (r *Renderer) Render(vm interface{}) (string, error) {
 		}
 		return "", err
 	}
+	fmt.Printf("RunScript took: %dms\n", time.Since(start).Milliseconds())
 
+	start = time.Now()
 	promise, err := renderResult.AsPromise()
 	if err != nil {
 		return "", err
 	}
 
-	return promise.Result().String(), nil
+	html := promise.Result().String()
+	fmt.Printf("AsPromise took: %dms\n", time.Since(start).Milliseconds())
+
+	return html, nil
 }
